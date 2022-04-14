5a9039ea853a0561d4b5f635d81021c076a36bf9
sys31337 <paytf@outlook.com>
2022-04-14 23:52:26 +0100

    Initializing

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const dotenv = require("dotenv");
+// const port = process.env.PORT || 3000
+const port = 3000
+
+dotenv.config();
+
+const app = express();
+
+// Parsing express requests objects as JSON objects
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Enabling CORS for browser clients
+app.all("*", (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
+
+app.use("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
