5b5910ab11aeb716092d4dbbf9bd6eb3b786111b
sys31337 <paytf@outlook.com>
2022-04-14 23:56:54 +0100

    Init

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
-// const port = process.env.PORT || 3000
-const port = 3000
+const port = process.env.PORT || 3000
 
 dotenv.config();
 
